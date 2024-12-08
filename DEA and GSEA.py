00c2a9ca126eeb08c8480f69a5d968686826a978
SSuperookie <102418112+SSuperookie@users.noreply.github.com>
2024-12-08 17:23:39 -0500

    Update DEA and GSEA.py

--- DEA and GSEA.py
+++ DEA and GSEA.py
@@ -83,8 +83,6 @@ adata.obs = meta_data
 
 # In[13]:
 
-
-# 过滤线粒体比例高的细胞，创建新的 AnnData 对象
 # adata = adata[adata.obs["pct_counts_mt"] < 5, :].copy()
 
 
@@ -99,7 +97,6 @@ adata.obs = meta_data
 # In[15]:
 
 
-# 高变基因筛选
 sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
 adata = adata[:, adata.var.highly_variable]
 
@@ -131,7 +128,7 @@ sc.pl.pca_variance_ratio(adata, log=True)
 
 
 # UMAP
-sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)  # 可调整 n_neighbors 和 n_pcs
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=40)  
 sc.tl.umap(adata)
 
 
